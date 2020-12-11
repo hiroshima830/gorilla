3c13a0bb0961f537b7615353c017eae68adf7af4
hiroshima830 <yomo0908@icloud.com>
2020-12-12 07:31:35 +0900

    外枠完成

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import random
+import flask
+from flask import render_template
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
 def main():
-    print("aaa")
+    omikuji = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"]
+    goribia = ["bia1", "bia2", "bia3", "bia4", "bia5", "bia6"]
+    result = omikuji[random.randint(0, len(omikuji))]
+    print(result)
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
