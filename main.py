0cb99ffc00754605c8ed85190e0ee0b3342ef003
hiroshima830 <yomo0908@icloud.com>
2020-12-12 23:14:42 +0900

    バック完成

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import flask
 from flask import render_template
 
@@ -8,9 +10,13 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def main():
-    omikuji = gorilla.no1
-    # result = omikuji[random.randint(0, len(omikuji))]
-    return render_template('index.html', omikuji=omikuji)
+    omikuji = [gorilla.no1, gorilla.no2, gorilla.no3, gorilla.no4, gorilla.no5, gorilla.no6,
+               gorilla.no7, gorilla.no8, gorilla.no9, gorilla.no10, gorilla.no11, gorilla.no12,
+               gorilla.no13, gorilla.no14, gorilla.no15, gorilla.no16, gorilla.no17, gorilla.no18,
+               gorilla.no19, gorilla.no20, gorilla.no21, gorilla.no22, gorilla.no23, gorilla.no24,
+               gorilla.no25]
+    result = omikuji[random.randrange(len(omikuji))]
+    return render_template('index.html', result=result)
 
 
 if __name__ == '__main__':
