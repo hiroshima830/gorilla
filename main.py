a9a961875d02e5616d55521011e5faa05318a35f
hiroshima830 <yomo0908@icloud.com>
2020-12-14 06:16:11 +0900

    bootstrapダウンロード

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ app = flask.Flask(__name__)
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result')
 def main():
     omikuji = [gorilla.no1, gorilla.no2, gorilla.no3, gorilla.no4, gorilla.no5, gorilla.no6,
                gorilla.no7, gorilla.no8, gorilla.no9, gorilla.no10, gorilla.no11, gorilla.no12,
@@ -16,7 +21,7 @@ def main():
                gorilla.no19, gorilla.no20, gorilla.no21, gorilla.no22, gorilla.no23, gorilla.no24,
                gorilla.no25]
     result = omikuji[random.randrange(len(omikuji))]
-    return render_template('index.html', result=result)
+    return render_template('result.html', result=result)
 
 
 if __name__ == '__main__':
